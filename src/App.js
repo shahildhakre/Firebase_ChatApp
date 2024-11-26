0ab7c57ffc0f448fffa31f9d93155feece1d7208
Sahil Dhakre <21ucs186@lnmiit.ac.in>
2024-11-26 09:15:10 +0530

    Structure and Design Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { Box, Button, Container, HStack, Input, VStack } from "@chakra-ui/react";
+import Message from "./Components/Message";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Box bg={"red.100"}>
+      <Container h={"100vh"} bg={"white"}>
+        <VStack h={"full"} paddingY={"4"}>
+          <Button w={"full"} colorScheme={"red"}>
+            Log Out
+          </Button>
+
+          <VStack h="full" w="full">
+            <Message text={"Sample Message"} user={"me"}/>
+            <Message text={"Sample Message"} />
+            <Message text={"Sample Message"} user={"me"}/>
+            <Message text={"Sample Message"} user={"me"}/>
+            <Message text={"Sample Message"} />
+            <Message text={"Sample Message"} />
+          </VStack>
+
+          <form style={{width: "100%"}}>
+            <HStack>
+              <Input placeholder="Enter a Message..."/>
+              <Button colorScheme={"purple"} type="submit">
+                Send
+              </Button>
+            </HStack>
+          </form>
+        </VStack>
+      </Container>
+    </Box>
   );
 }
 
